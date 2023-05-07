1d1060eafdba69a5a879033b35e28f0d852e6674
Ste <n00202054@iadt.ie>
2023-05-07 19:05:22 +0100

    mui-gradient-text

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import TextGradient from './components/TextGradient';
+
+const App = () => {
+
+  let options = {
+    firstColor: "#4c82ed", //first color 
+    secondColor: "#FF6767", //second color
+    direction: "left", //top, bottom, left, right, degrees
+    variant: 'h4' //body1, body2, 'body1', 'body2', 'button', 'caption', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'inherit', 'overline', 'subtitle1', 'subtitle2'
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      
+    <TextGradient options={options}>
+      Whereas disregard and contempt for human rights have resulted
+    </TextGradient>
+     
   );
 }
 
---
+++ src/components/TextGradient.js
@@ -0,0 +1,25 @@
+import { Typography } from "@mui/material"
+
+
+const TextGradient = ({options, children}) => {
+
+   
+
+    return (
+        <Typography
+            variant={options.variant}
+            sx={{
+                backgroundImage: `linear-gradient( to ${options.direction}, ${options.firstColor}, ${options.secondColor})`,
+                backgroundSize: "100%",
+                backgroundRepeat: "repeat",
+                backgroundClip: "text",
+                WebkitBackgroundClip: "text",
+                WebkitTextFillColor: "transparent"
+            }}
+        >
+            {children}
+        </Typography>
+    )
+
+}
+export default TextGradient
